4092b44e908e74fbd6dfb127798a208430c8a0de
AHmims <ali.hmims99@gmail.com>
2021-01-18 14:04:10 +0100

    creation d'interface login

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("interfaces/authentification.fxml"));
         primaryStage.setTitle("Youcode absence");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("res/img/favicon.png")));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
---
+++ src/sample/interfaces/Authentification.java
@@ -0,0 +1,20 @@
+package sample.interfaces;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Authentification {
+    @FXML
+    TextField input_email;
+    @FXML
+    PasswordField input_pass;
+    @FXML
+    Button btn_login;
+    //
+    @FXML
+    public void login(){
+
+    }
+}
