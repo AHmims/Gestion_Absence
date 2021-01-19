32f178d7ae054fd56dff0f8767aa73e8c90ef876
AHmims <ali.hmims99@gmail.com>
2021-01-19 13:13:59 +0100

    admin navbar

--- src/sample/interfaces/Authentification.java
+++ src/sample/interfaces/Authentification.java
@@ -1,11 +1,19 @@
 package sample.interfaces;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Authentification {
+    @FXML
+    Pane root;
     @FXML
     TextField input_email;
     @FXML
@@ -15,6 +23,17 @@ public class Authentification {
     //
     @FXML
     public void login(){
-
+        switchScene();
+    }
+    //Method called when user authenticates successfully
+    private void switchScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("admin.fxml"));
+            Stage stage = (Stage) root.getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
